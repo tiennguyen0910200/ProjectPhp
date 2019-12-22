78917cd7e0b36873fbfbe6e6752f997342683ad1
tiennguyen0910200 <55585099+tiennguyen0910200@users.noreply.github.com>
2019-12-22 16:49:16 +0700

    Add files via upload

---
+++ ProjectPhp/Php/TrangChu.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>rice store</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../Css/TrangChu.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<div class="header1">
+				<div class="flex">
+					<ul class="flex1">
+						<li><img src="../Img/telephone.png">(+84)983 942 707</li>
+						<li><img src="../Img/map.png">Xuân Phú - Hòa Sơn - Hòa Vang - TP. Đà Nẵng</li>
+						<li><img src="../Img/fb.png">Dàn Nhạc Trịnh</li>
+					</ul>
+					<ul class="flex2">
+						<li><img src="../Img/user.png">Đăng kí</li>
+						<li><img src="../Img/user.png">Đăng nhập</li>
+					</ul>
+				</div>
+			</div>
+			<hr>
+			<div class="header2">
+				<div>
+					<img width="150px" height="150px" src="../Img/logo.png">
+					
+				</div>
+                <div class="menu">
+				<div>
+					<img src="../Img/home.png">
+					<br>
+					<h4>Trang chủ</h4>
+				</div>
+				<div>
+					<img src="../Img/contact.png">
+					<br>
+					<h4>Liên hệ</h4>
+				</div>
+				<div>
+					<img src="../Img/phone.png">
+					<br>
+					<h4>Tư vấn</h4>
+				</div>
+				<div>
+					<img src="../Img/percen.png">
+					<br>
+					<h4>Khuyến mãi</h4>
+				</div>
+				<div>
+					<img src="../Img/detail.png">
+					<br>
+					<h4>Chi tiết</h4>
+				</div>
+				<div>
+					<img src="../Img/time.png">
+					<br>
+					<h4>Thời gian</h4>
+				</div>
+				<div>
+					<img src="../Img/new.png">
+					<br>
+					<h4>Dịch vụ mới</h4>
+				</div>
+				<div>
+					<img src="../Img/more.png">
+					<br>
+					<h4>Khác</h4>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="menubar">
+			<ul>
+				<li><a href="#">ĐÁM CƯỚI</a></li>
+				<li><a href="#">TIỆC</a></li>
+				<li><a href="#">LIÊN HOAN</a>
+					<ul class="sub-menu">
+						<li><a href="#">ABC</a></li>
+						<li><a href="#">BCD</a></li>
+						<li><a href="#">CDE</a></li>
+					</ul>
+				</li>
+				<li style="margin-right: 500px;"><a href="#">KHÁC</a></li>
+				<input autocomplete="off" class="search" placeholder="Tìm kiếm bài viết" spellcheck="false" type="text">	
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html> 
